beb78dda469e1b0c0bb3ff768884f5b0c349060c
vrvs <vrvs@cin.ufpe.br>
2019-08-19 12:00:31 -0300

    2-5 done

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -2,12 +2,156 @@ package br.ufpe.cin.android.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    // Aqui estamos salvando o estado para quando ele destruir a activity
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        val editText : EditText = findViewById(R.id.text_calc)
+        val textView : TextView = findViewById(R.id.text_info)
+
+        outState?.putString(R.id.text_calc.toString(), editText.text.toString())
+        outState?.putString(R.id.text_info.toString(), textView.text.toString())
+    }
+
+    //Aqui estamos recuperando o estado para pegar as informações antes da mudança de configuração
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val editText : EditText = findViewById(R.id.text_calc)
+        val textView : TextView = findViewById(R.id.text_info)
+
+        val editTextValue : String? = savedInstanceState?.getString(R.id.text_calc.toString(), "")
+        val textViewValue : String? = savedInstanceState?.getString(R.id.text_info.toString(), "")
+
+        editText.setText(editTextValue)
+        textView.text = textViewValue
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Aqui estou setando os listeners de cada botão para escrever as informações na tela
+        btn_0.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("0")
+        }
+
+        btn_1.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("1")
+        }
+
+        btn_2.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("2")
+        }
+
+        btn_3.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("3")
+        }
+
+        btn_4.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("4")
+        }
+
+        btn_5.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("5")
+        }
+
+        btn_6.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("6")
+        }
+
+        btn_7.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("7")
+        }
+
+        btn_8.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("8")
+        }
+
+        btn_9.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("9")
+        }
+
+        btn_Dot.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append(".")
+        }
+
+        btn_Add.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("+")
+        }
+
+        btn_Subtract.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("-")
+        }
+
+        btn_Divide.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("/")
+        }
+
+        btn_Multiply.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("*")
+        }
+
+        btn_Power.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("^")
+        }
+
+        btn_LParen.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append("(")
+        }
+
+        btn_RParen.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            editText.append(")")
+        }
+
+        // Aqui estou fazendo o clear para limpar o EditText e o TextView
+        btn_Clear.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            val textView : TextView = findViewById(R.id.text_info)
+            editText.setText("")
+            textView.text = ""
+        }
+
+        // Aqui estou fazendo o calculo da expressão e colocando no TextView e, caso seja inválida, aparecer um toast informando o usuário
+        btn_Equal.setOnClickListener {
+            val editText : EditText = findViewById(R.id.text_calc)
+            val textView : TextView = findViewById(R.id.text_info)
+            try {
+                val calculated : Double = eval(editText.text.toString())
+                textView.text = calculated.toString()
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this,
+                    "Expressão Inválida!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
 
